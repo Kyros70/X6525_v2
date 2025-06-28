e5cb1e780cc7ff9731580e68c3fa763b8338da17
Massatrio16 <dimassetosatrio@gmail.com>
2025-06-28 19:15:42 +0700

    update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,10 @@ BOARD_RAMDISK_USE_LZ4 := true
 # Assert
 TARGET_OTA_ASSERT_DEVICE := Infinix-X6525
 
+# Dynamic Partition
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# System as root
+BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Architecture
 TARGET_ARCH := arm64
@@ -167,6 +171,8 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_DEVICE_VERSION := X6525-Massatrio16
+TW_INCLUDE_NTFS_3G := true
+TARGET_USES_MKE2FS := true
 
 #logs
 TARGET_USES_LOGD := true
