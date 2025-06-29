82dfd0b819dce5922f8b18c5775db43a868bef94
Massatrio16 <dimassetosatrio@gmail.com>
2025-06-29 07:03:49 +0700

    add pbrp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_X6525.mk
+    $(LOCAL_DIR)/twrp_X6525.mk \
+    $(LOCAL_DIR)/pb_X6525.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_X6525-eng
+    twrp_X6525-eng \
+    pb_X6525-eng
